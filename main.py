8c52a6390267f5a9b920de11963340e007657109
jestern001 <nathan.d.jester@gmail.com>
2024-03-20 22:43:55 -0400

    added a view to the window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from tkinter import Canvas, Tk
 
-from tkinter import Tk
+from colors import Colors
 
 
 BASE_SIZE = 16
@@ -7,17 +8,23 @@ WINDOW_HEIGHT = 240
 WINDOW_WIDTH = 256
 
 
-class Game(Tk):
+class Window(Tk):
 
     def run(
         self, 
         window_height: int = WINDOW_HEIGHT, 
         window_width: int = WINDOW_WIDTH
     ):
+
+        # add a view
+        view = Canvas(self, bg=Colors.RED)
+        view.pack()
+
+        # run
         self.geometry(f"{window_width}x{window_height}")
         self.mainloop()
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = Window()
     game.run()
