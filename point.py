05c7896350895c8c68506a58c399f15225298627
jestern001 <nathan.d.jester@gmail.com>
2024-04-01 20:31:11 -0400

    added point add and multiply methods

--- point.py
+++ point.py
@@ -5,3 +5,9 @@ from dataclasses import dataclass
 class Point:
     x: float
     y: float
+
+    def __add__(self, offset: 'Point'):
+        return Point(self.x+offset.x, self.y+offset.y)
+    
+    def __mul__(self, scale: float):
+        return Point(self.x*scale, self.y*scale)
