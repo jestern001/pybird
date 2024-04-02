e2f0eb607290cbdf795ea7e78e6ace34cca85580
jestern001 <nathan.d.jester@gmail.com>
2024-04-01 20:50:50 -0400

    Updated player to stay in the room

--- room.py
+++ room.py
@@ -48,13 +48,28 @@ class Room:
     
     def move_obj(self, obj: int, offset: Point):
         self.canvas.move(obj, offset.x, offset.y)
+    
+    def get_location(self, obj: int) -> tuple[Point, Point]:
+        """Get top corner and size as points
+
+        Args:
+            obj (int): The object ID
+
+        Returns:
+            tuple[Point, Point]: The top left corner as a point and height and width as a point
+        """
+        x0, y0, x1, y1 = self.canvas.coords(obj)
+        return (Point(x0, y0), Point(x1-x0, y1-y0))
 
     def get_from_tag(self, tag: str) -> list[int]:
         return list(self.canvas.find_withtag(tag))
 
     def after(self, speed: float, method: Callable):
         self.canvas.after(speed, method)
-
+    
+    def get_room_size(self) -> Point:
+        return Point(self.canvas.winfo_reqwidth(), self.canvas.winfo_reqwidth())
+    
     def move_player(self):
         # get player
         player = self.get_from_tag(Tags.PLAYER)
@@ -71,6 +86,17 @@ class Room:
             offset += Point(0, -1)
         if self.inputs.key_pressed(Keys.DOWN):
             offset += Point(0, 1)
+        
+        # check new location
+        room_size = self.get_room_size()
+        location, size = self.get_location(player)
+        new_location = location + offset
+
+        # stay in room
+        if not (0 < new_location.x < room_size.x - size.x):
+            offset.x = 0
+        if not (0 < new_location.y < room_size.y - size.y):
+            offset.y = 0
         
         # move player
         self.move_obj(player, offset * self.move_speed)
